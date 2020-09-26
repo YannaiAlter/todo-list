932297efbd80b3837f894f3f3de27695d0c49ed6
yannai alter <yannaialter@MacBook-Air.local>
2020-09-26 23:21:08 +0300

    Update connectionConfig to have mongodb option

--- connectionConfig.ts
+++ connectionConfig.ts
@@ -1,16 +1,27 @@
-import { createConnection } from "typeorm";
+import { ConnectionOptions, createConnections } from "typeorm";
 
-export const createDBConnection = () =>
-  createConnection({
-    url: process.env.DATABASE_URL,
+const connections: ConnectionOptions[] = [];
+const postgresqlConfig: ConnectionOptions = {
+  url: process.env.DATABASE_URL,
+  type: "postgres",
+  synchronize: true,
+  ssl: false,
+  entities: ["build/src/typeorm/Todo/TodoEntity.js"],
+  logging: false,
+};
+
+connections.push(postgresqlConfig);
+
+if (process.env.BACKUP_DATABASE_URL) {
+  const mongodbBackupConfig: ConnectionOptions = {
+    url: process.env.BACKUP_DATABASE_URL,
     type: "postgres",
     synchronize: true,
-    ssl: true,
+    ssl: false,
     entities: ["build/src/typeorm/Todo/TodoEntity.js"],
     logging: false,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  });
+  };
+  connections.push(mongodbBackupConfig);
+}
+
+export const createDBConnection = () => createConnections(connections);
