5c7ac11c2788347f07a0411a35e8b396d59339da
yannai alter <yannaialter@MacBook-Air.local>
2020-09-27 16:56:27 +0300

    Add cors support

--- app.ts
+++ app.ts
@@ -7,20 +7,17 @@ import cors from "cors";
 createDBConnection()
   .then((connection) => {
     const app: express.Application = express();
+    app.use(cors());
     const { todos } = require("./src/routes/todos");
     app.use(bodyParser.json());
 
     //api/todos
-    app.use("/api", todos, cors());
+    app.use("/api", todos);
 
     //api
-    app.get(
-      "/api",
-      function (req, res) {
-        res.send("Hello World!");
-      },
-      cors()
-    );
+    app.get("/api", function (req, res) {
+      res.send("Hello World!");
+    });
     app.listen(process.env.PORT || 3000, function () {
       console.log("App is listening on port 3000!");
     });
